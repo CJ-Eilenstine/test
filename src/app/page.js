6459686c4d2ca639d2cf56af98f0f8a89c4301d0
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-09-09 21:15:40 -0400

    Nav
    I finished the navigation with 3 links tonight.

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,22 @@
+"use client";
+import homeStyles from "@/app/page.module.css";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <nav className={homeStyles.homeNav}>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </main>
+  );
 }
